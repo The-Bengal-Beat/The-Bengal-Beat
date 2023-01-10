9930c25f51a066f150cce0a03088912661a8d5c2
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-10 16:11:08 +0000

    fixed error message

--- src/pages/api/google-form.ts
+++ src/pages/api/google-form.ts
@@ -2,12 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
-        res.status(405).send({ 
-            data: [],
-            errors: [{
-                message: 'Only POST requests allowed'
-            }]
-        })
+        res.status(405).send({ message: 'Only POST requests allowed' })
     }
 
 
