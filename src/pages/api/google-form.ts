6cf0d4d48b40fdd0ffd050bd5900244176c41597
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-10 16:22:28 +0000

    assigning to RequestBody type

--- src/pages/api/google-form.ts
+++ src/pages/api/google-form.ts
@@ -1,10 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+export interface RequestBody {
+    text: "hello"
+}
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
         res.status(405).send({ message: 'Only POST requests allowed' })
     }
 
-
-    res.status(200).json(req.body)
+    res.status(200).json(req.body as RequestBody)
 }
