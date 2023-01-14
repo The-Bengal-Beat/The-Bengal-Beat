ba559a9834ba12a672b80464d4fe2b4794322fb5
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-14 06:43:22 +0000

    Prisma database

--- src/pages/api/google-form.ts
+++ src/pages/api/google-form.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
+import { PrismaClient } from '@prisma/client'
+
 
 export const CategoryEnum = [
     "Athletics",
@@ -33,12 +35,25 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     }
 
     if (RequestSchema.safeParse(req.body).success) {
-        res.status(200).json({
-            data: [req.body],
-            success: true,
-            errors: [],
-
-        });
+        try {
+            const prisma = new PrismaClient()
+            prisma.article.create({ data: req.body })
+            
+            res.status(200).json({
+                data: [req.body],
+                success: true,
+                errors: [],
+    
+            });
+        } catch (err) {
+            res.status(400).json({
+                data: [],
+                success: false,
+                errors: [{
+                    message: "Prisma error"
+                }]
+            })
+        }
     } else res.status(400).json({
         data: [],
         success: false,
