731ade21450628594455765630026f6ff30e8b04
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-15 00:47:48 +0000

    removed useless code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { type NextPage } from "next";
-import { readFileSync } from "fs";
-import { IArticle } from "./api/google-form";
 
-const Home: NextPage<{ articles: IArticle[] }> = ({ articles }) => {
+const Home: NextPage = () => {
   return (
     <>
     </>
