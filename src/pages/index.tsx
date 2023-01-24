89794973984fbb00f619f03fbb3dbd132c1ae028
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-24 16:50:11 +0000

    implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,21 @@ import { useState } from "react";
 import type { NextPage } from "next";
 import type { IPostFromApi, State } from "../types";
 import axios from "axios";
+import { getDataFromApi } from "../utils/getDataFromApi";
 
-const onClick = ([_, setState]: State<{ data: string, error: string }>) => {
-  axios.get("https://thebengalbeat.com/wp-json/wp/v2/posts")
-    .then(response => {
-      
-      setState({ data: JSON.stringify(response.data as IPostFromApi[]), error: "" })
-    })
-    .catch(err => {
-      setState({ data: "", error: JSON.stringify(err) })
-    })
+const onClick = ([_, setState]: State<{ data: IPostFromApi[], error: string }>) => {
+  getDataFromApi().then(data => setState(data));
 }
 
 const Home: NextPage = () => {
-  const [state, setState] = useState({ data: "", error: "" })
+  const [state, setState] = useState({ data: [], error: "" })
   
   return (
     <>
       <Head>
         <title>Home Page</title>
       </Head>
-      <p>Data: {state.data}</p>
+      <p>Data: {JSON.stringify(state.data)}</p>
       <p>Errors: {state.error}</p>
       <button onClick={() => onClick([state, setState])}>Get Values</button>
     </>
