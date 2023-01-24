2ea743f420226566c46b1f45428b7f97ab0aa30c
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-24 16:12:18 +0000

    wordpress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import type { State } from "../types";
 import axios from "axios";
 
 const onClick = ([_, setState]: State<{ data: string, error: string }>) => {
-  axios.get("https://the-bengal-beat.vercel.app/api/responses")
+  axios.get("https://thebengalbeat.com/wp-json/wp/v2/posts")
     .then(response => {
       setState({ data: JSON.stringify(response.data), error: "" })
     })
