afa4b67202f5ad774962df12a0b005bee13306ac
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-25 15:08:09 +0000

    overflow

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -8,8 +8,8 @@ interface IPostProps {
 const Post = ({ data }: IPostProps) => {
   return (
     <div className="flex w-[300px] h-[500px]">
-        <h3 dangerouslySetInnerHTML={{ __html: data.title.rendered }} />
-        <p dangerouslySetInnerHTML={{ __html: data.content.rendered }} />
+        <h3 dangerouslySetInnerHTML={{ __html: data.title.rendered }} className="overflow-none" />
+        <p dangerouslySetInnerHTML={{ __html: data.content.rendered }} className="overflow-none" />
     </div>
   )
 }
