53b16553f956da08515f95c550f67213e0d935dc
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-25 15:05:37 +0000

    Dangerously inset html

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -8,8 +8,8 @@ interface IPostProps {
 const Post = ({ data }: IPostProps) => {
   return (
     <div className="flex w-[300px] h-[500px]">
-        <h3>{data.title.rendered}</h3>
-        <p>{data.content.rendered}</p>
+        <h3 dangerouslySetInnerHTML={{ __html: data.title.rendered }} />
+        <p dangerouslySetInnerHTML={{ __html: data.content.rendered }} />
     </div>
   )
 }
