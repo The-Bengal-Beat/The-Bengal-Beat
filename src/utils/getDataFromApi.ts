b98ce3ae8e77e2976b1a705220ec2527928e9ce1
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-26 15:55:24 +0000

    increased limit

--- src/utils/getDataFromApi.ts
+++ src/utils/getDataFromApi.ts
@@ -8,7 +8,7 @@ export interface IApiOutput {
 }
 
 export const getDataFromApi = async () => {
-    const response = await axios.get("https://thebengalbeat.com/wp-json/wp/v2/posts")
+    const response = await axios.get("https://thebengalbeat.com/wp-json/wp/v2/posts?per_page=1000")
     
     // parsing
     const parsed = PostArrayFromApiSchema.safeParse(response.data)
