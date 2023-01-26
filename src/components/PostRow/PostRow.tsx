8b2ad932a2c6b89faf2cfe402249c9aa1817fad8
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-26 15:13:39 +0000

    fill in content (except: author)

--- src/components/PostRow/PostRow.tsx
+++ src/components/PostRow/PostRow.tsx
@@ -9,11 +9,11 @@ const PostRow: React.FC<IPostRow> = ({ post }) => {
   return (
     <div className="flex flex-row w-full h-50px p-2 m-4 bg-[#363636]">
         <div className="">
-            <p className="text-white text-4">F. Last Name</p>
-            <p className="text-white text-4">Title of the Article</p>
+            <p className="text-white text-4 font-semibold">F. Last Name</p>
+            <p className="text-white text-4">{post?.title.rendered}</p>
         </div>
         <div className="flex-grow"/>
-        <p className="text-white text-4">1/26/23</p>
+        <p className="text-white text-4">{post?.date_gmt}</p>
     </div>
   )
 }
