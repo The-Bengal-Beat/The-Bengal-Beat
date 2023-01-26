a9c0b51f4a416c0a6db5698dd2dec96bc80e0d7c
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-26 16:32:04 +0000

    headers

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,4 +6,5 @@ export type State<T> = [T, Dispatch<SetStateAction<T>>]
 export type IPostFromApi = z.infer<typeof PostFromApiSchema>
 export type IHeaders = AxiosResponseHeaders | Partial<Record<string, string> & {
     "set-cookie"?: string[] | undefined;
+    "X-WP-Total"?: string
 }>
