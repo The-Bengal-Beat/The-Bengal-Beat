295e3b10a1499c6399047f3079a4665b250d79b2
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-30 17:26:59 +0000

    box -> paper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { Box } from "@mui/material";
+import { Paper } from "@mui/material";
 import type { NextPage } from "next";
 import PostTable from "../components/PostTable/PostTable";
 
 const Home: NextPage = () => {
   return (
-    <Box className="p-16">
+    <Paper className="p-16">
       <PostTable />
-    </Box>
+    </Paper>
   )
 };
 
