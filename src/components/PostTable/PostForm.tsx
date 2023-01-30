6da8fb980d42f9f956cf2d503ba8887064d4e2cf
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-30 17:49:20 +0000

    spacer

--- src/components/PostTable/PostForm.tsx
+++ src/components/PostTable/PostForm.tsx
@@ -6,12 +6,12 @@ export const PostForm = () => {
   const { control } = useForm();
 
   return (
-    <div className="p-4 w-full flex flex-row justify-start">
+    <div className="py-4 px-2 w-full flex flex-row justify-start">
       <Controller
         name="writer"
         control={control}
         render={({ field }) => (
-          <FormControl className="w-[200px] px-2 py-4">
+          <FormControl className="w-[200px]">
             <InputLabel id="writer-input">Writer</InputLabel>
             <Select labelId="writer-input" label="Writer" {...field}>
               <MenuItem value="Writer One">Writer One</MenuItem>
@@ -21,11 +21,12 @@ export const PostForm = () => {
           </FormControl>
         )}
       />
+      <div className="w-2" />
       <Controller
         name="category"
         control={control}
         render={({ field }) => (
-          <FormControl className="w-[200px] px-2 py-4">
+          <FormControl className="w-[200px]">
             <InputLabel id="category-input">Category</InputLabel>
             <Select labelId="category-input" label="Category" {...field}>
               <MenuItem value="Category One">Category One</MenuItem>
