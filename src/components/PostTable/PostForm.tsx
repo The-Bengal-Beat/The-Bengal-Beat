ad4aaba4910b28c1b60e3eabdedbcc12960bb862
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-01-30 16:36:04 +0000

    label

--- src/components/PostTable/PostForm.tsx
+++ src/components/PostTable/PostForm.tsx
@@ -1,32 +1,38 @@
-import { MenuItem, Select } from "@mui/material";
+import { InputLabel, MenuItem, Select } from "@mui/material";
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
 
 export const PostForm = () => {
-  const { control } = useForm()
+  const { control } = useForm();
 
   return (
-    <div className="m-4 flex flex-row items-start">
+    <div className="m-4 flex flex-row justify-start">
       <Controller
         name="writer"
         control={control}
         render={({ field }) => (
-          <Select label="Writer" {...field}>
-            <MenuItem value="Writer One">Writer One</MenuItem>
-            <MenuItem value="Writer Two">Writer Two</MenuItem>
-            <MenuItem value="Writer Three">Writer Three</MenuItem>
-          </Select>
+          <>
+            <InputLabel id="writer-input">Writer</InputLabel>
+            <Select labelId="writer-input" label="Writer" {...field}>
+              <MenuItem value="Writer One">Writer One</MenuItem>
+              <MenuItem value="Writer Two">Writer Two</MenuItem>
+              <MenuItem value="Writer Three">Writer Three</MenuItem>
+            </Select>
+          </>
         )}
       />
       <Controller
         name="category"
         control={control}
         render={({ field }) => (
-          <Select label="Category" {...field}>
-            <MenuItem value="Category One">Category One</MenuItem>
-            <MenuItem value="Category Two">Category Two</MenuItem>
-            <MenuItem value="Category Three">Category Three</MenuItem>
-          </Select>
+          <>
+            <InputLabel id="category-input">Category</InputLabel>
+            <Select labelId="category-input" label="Category" {...field}>
+              <MenuItem value="Category One">Category One</MenuItem>
+              <MenuItem value="Category Two">Category Two</MenuItem>
+              <MenuItem value="Category Three">Category Three</MenuItem>
+            </Select>
+          </>
         )}
       />
     </div>
