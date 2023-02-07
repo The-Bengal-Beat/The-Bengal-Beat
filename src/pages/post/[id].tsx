2be9284bcdc051c87293738ff8ea626b1a1221a7
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-02-07 16:24:41 +0000

    page & function

---
+++ src/pages/post/[id].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Post: NextPage = () => {
+  return (
+    <div className="p-16 h-full w-full">
+      Hello
+    </div>
+  )
+};
+
+export default Post;
