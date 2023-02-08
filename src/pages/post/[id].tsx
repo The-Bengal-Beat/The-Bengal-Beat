f546888c516db6f42d6564685fa7f0d88b53c03a
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-02-08 14:37:38 +0000

    render title

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,9 +1,25 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { getPost, IGetPostResponse } from "../../utils/getPost";
+import { parseHTML } from "../../utils/parseHTML";
 
 const Post: NextPage = () => {
+  const router = useRouter()
+  const { id } = router.query;
+
+  if (!(typeof id === "string")) return <></>;
+
+  const [post, setPost] = useState<IGetPostResponse>();
+
+  useEffect(() => {
+    getPost(id)
+      .then((data) => setPost(data))
+  }, [])
+
   return (
     <div className="p-16 h-full w-full">
-      Hello
+      {parseHTML(post?.data.title.rendered)}
     </div>
   )
 };
